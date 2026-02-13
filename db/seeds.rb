fe44fd73fe0ba4c6ce5b0206075305ef2c12f695
Stoyan <stoyan_stoyanov@rocketmail.com>
2026-02-14 00:22:47 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+file_path = Rails.root.join('db', 'seeds', 'data.json')
+
+
+trips_data = JSON.parse(File.read(file_path))['trips']
+
+
+bulk_trips = trips_data.map do |trip|
+  {
+      name: trip['name'],
+      image_url: trip['image'],
+      short_description: trip['short_description'] || trip['description'],
+      long_description: trip['long_description'],
+      rating: trip['rating'],
+      created_at: Time.current,
+      updated_at: Time.current
+  }
+end
+  
+Trip.upsert_all(bulk_trips, unique_by: :name)
+
+puts "✅ Successfully seeded #{bulk_trips.size} trips into the database."
