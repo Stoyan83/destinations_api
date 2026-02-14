b255ec790955501fcb99c0833ae4f9c56e1d7d31
Stoyan <stoyan_stoyanov@rocketmail.com>
2026-02-14 11:38:19 +0200

    Move default per page in kaminari config

--- app/controllers/concerns/paginatable.rb
+++ app/controllers/concerns/paginatable.rb
@@ -2,17 +2,14 @@ module Paginatable
   extend ActiveSupport::Concern
 
   def paginate(scope, page: params[:page], per_page: params[:per_page])
-    page_number = (page || 1).to_i
-    per_page_number = (per_page || 10).to_i
-
-    paginated = scope.page(page_number).per(per_page_number)
+    paginated = scope.page(page).per(per_page)
 
     {
       data: paginated,
       meta: {
-        current_page: paginated.current_page,
-        per_page: paginated.limit_value,
-        total_pages: paginated.total_pages,
+        current_page:  paginated.current_page,
+        per_page:      paginated.limit_value,
+        total_pages:   paginated.total_pages,
         total_records: paginated.total_count
       }
     }
