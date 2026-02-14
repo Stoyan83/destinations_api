2a2de7689e8d632e4f789015a1102e0ca32b73a0
Stoyan <stoyan_stoyanov@rocketmail.com>
2026-02-14 08:07:48 +0200

    Add pg_search gem

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem "thruster", require: false
 gem "sidekiq"
 gem "redis"
 
+# Full-text, trigram & fuzzy search in PostgreSQL
+gem "pg_search"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,19 @@
 class Trip < ApplicationRecord
+  include PgSearch::Model
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
   validates :image_url, presence: true, length: { maximum: 65_535 }
   validates :short_description, presence: true, length: { maximum: 65_535 }
   validates :long_description, length: { maximum: 65_535 }, allow_nil: true
   validates :rating, presence: true, inclusion: { in: 1..5, message: :rating_range }
+
+  pg_search_scope :combined_search,
+    against: {
+      name: 'A'  
+    },
+    using: {
+      tsearch: { prefix: true },  
+      trigram: { threshold: 0.4 }
+    },
+    ranked_by: ":tsearch + (0.5 * :trigram)"
 end
